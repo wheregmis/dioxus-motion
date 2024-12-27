8d48d42e3a183a736033b3d8bc797f2bac797539
Sabin Regmi <get2sabin@gmail.com>
2024-12-27 17:57:34 -0500

    Remove Experimental Code

--- example_projects/src/components/mod.rs
+++ example_projects/src/components/mod.rs
@@ -2,5 +2,3 @@ pub mod spring_component;
 pub mod timer_component;
 pub use spring_component::SpringBoxComponent;
 pub use timer_component::TimerComponent;
-pub mod motion_div;
-pub use motion_div::MotionDiv;
