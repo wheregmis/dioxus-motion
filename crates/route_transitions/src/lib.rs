3939bd7b603c7a63c7a8dd43c2b8c272501f8be7
Sabin Regmi <get2sabin@gmail.com>
2025-03-06 11:32:40 -0500

    Adding Layout Depth

--- crates/route_transitions/src/lib.rs
+++ crates/route_transitions/src/lib.rs
@@ -15,20 +15,39 @@ fn get_transition_from_attrs(attrs: &[Attribute]) -> Option<String> {
         })
 }
 
-fn get_layout_from_attrs(attrs: &[Attribute]) -> Option<syn::Path> {
-    attrs
-        .iter()
-        .find(|attr| attr.path().is_ident("layout"))
-        .and_then(|attr| {
-            if let Ok(Meta::Path(path)) = attr.parse_args::<Meta>() {
-                Some(path.clone())
-            } else {
-                None
-            }
-        })
+// Helper to extract layout nesting information from enum variants
+fn get_layout_depth(variants: &[&syn::Variant]) -> Vec<(syn::Ident, usize)> {
+    let mut layout_depth = Vec::new();
+    let mut current_depth = 0;
+
+    for variant in variants {
+        // Check if this variant has a layout attribute
+        if variant
+            .attrs
+            .iter()
+            .any(|attr| attr.path().is_ident("layout"))
+        {
+            current_depth += 1;
+        }
+
+        // Check if this variant ends a layout
+        if variant
+            .attrs
+            .iter()
+            .any(|attr| attr.path().is_ident("end_layout"))
+            && current_depth > 0
+        {
+            current_depth -= 1;
+        }
+
+        // Associate current depth with this variant
+        layout_depth.push((variant.ident.clone(), current_depth));
+    }
+
+    layout_depth
 }
 
-#[proc_macro_derive(MotionTransitions, attributes(transition))]
+#[proc_macro_derive(MotionTransitions, attributes(transition, layout, end_layout))]
 pub fn derive_route_transitions(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
@@ -36,6 +55,28 @@ pub fn derive_route_transitions(input: TokenStream) -> TokenStream {
         Data::Enum(DataEnum { variants, .. }) => variants,
         _ => panic!("MotionTransitions can only be derived for enums"),
     };
+
+    let component_match_arms = variants.iter().map(|variant| {
+        let variant_ident = &variant.ident;
+        let component_name = &variant.ident;
+
+        match &variant.fields {
+            Fields::Named(fields) => {
+                let field_names: Vec<_> = fields.named.iter().map(|f| &f.ident).collect();
+                quote! {
+                    Self::#variant_ident { #(ref #field_names,)* } => {
+                        rsx! { #component_name { #(#field_names: #field_names.clone(),)* } }
+                    }
+                }
+            }
+            Fields::Unnamed(_) => {
+                quote! { Self::#variant_ident(..) => rsx! { #component_name {} } }
+            }
+            Fields::Unit => {
+                quote! { Self::#variant_ident {} => rsx! { #component_name {} } }
+            }
+        }
+    });
 
     let transition_match_arms = variants.iter().map(|variant| {
         let variant_ident = &variant.ident;
@@ -62,64 +103,33 @@ pub fn derive_route_transitions(input: TokenStream) -> TokenStream {
         }
     });
 
-    let component_match_arms = variants.iter().map(|variant| {
-        let variant_ident = &variant.ident;
-        let component_name = &variant.ident;
-
-        match &variant.fields {
-            Fields::Named(fields) => {
-                let field_names: Vec<_> = fields.named.iter().map(|f| &f.ident).collect();
-                quote! {
-                    Self::#variant_ident { #(ref #field_names,)* } => {
-                        rsx! { #component_name { #(#field_names: #field_names.clone(),)* } }
-                    }
-                }
-            }
-            Fields::Unnamed(_) => {
-                quote! { Self::#variant_ident(..) => rsx! { #component_name {} } }
-            }
-            Fields::Unit => {
-                quote! { Self::#variant_ident {} => rsx! { #component_name {} } }
-            }
-        }
-    });
-
-    let layout_match_arms = variants.iter().map(|variant| {
-        let variant_ident = &variant.ident;
-        let layout = get_layout_from_attrs(&variant.attrs);
-
-        match &variant.fields {
-            Fields::Named(fields) => {
-                let field_patterns = fields.named.iter().map(|f| {
-                    let name = &f.ident;
-                    quote! { #name: _ }
-                });
-                if let Some(layout_path) = layout {
+    // Generate layout depth match arms
+    let layout_depths = get_layout_depth(&variants.iter().collect::<Vec<_>>());
+    let layout_depth_match_arms =
+        layout_depths.iter().map(|(variant_ident, depth)| {
+            match &variants
+                .iter()
+                .find(|v| &v.ident == variant_ident)
+                .unwrap()
+                .fields
+            {
+                Fields::Named(fields) => {
+                    let field_patterns = fields.named.iter().map(|f| {
+                        let name = &f.ident;
+                        quote! { #name: _ }
+                    });
                     quote! {
-                        Self::#variant_ident { #(#field_patterns,)* } => Some(rsx! { #layout_path {} })
-                    }
-                } else {
-                    quote! {
-                        Self::#variant_ident { #(#field_patterns,)* } => None
+                        Self::#variant_ident { #(#field_patterns,)* } => #depth
                     }
                 }
-            }
-            Fields::Unnamed(_) => {
-                if let Some(layout_path) = layout {
-                    quote! { Self::#variant_ident(..) => Some(rsx! { #layout_path {} }) }
-                } else {
-                    quote! { Self::#variant_ident(..) => None }
+                Fields::Unnamed(_) => {
+                    quote! { Self::#variant_ident(..) => #depth }
+                }
+                Fields::Unit => {
+                    quote! { Self::#variant_ident {} => #depth }
                 }
             }
-            Fields::Unit => {
-                if let Some(layout_path) = layout {
-                    quote! { Self::#variant_ident => Some(rsx! { #layout_path {} }) }
-                } else {
-                    quote! { Self::#variant_ident => None }
-                }
-            }
-        }
-    });
+        });
 
     let expanded = quote! {
         impl AnimatableRoute for  #name {
@@ -136,15 +146,14 @@ pub fn derive_route_transitions(input: TokenStream) -> TokenStream {
                 }
             }
 
-            fn get_layout(&self) -> Option<Element> {
+            // New method to get layout depth
+            fn get_layout_depth(&self) -> usize {
                 match self {
-                    #(#layout_match_arms,)*
-                    _ => None,
+                    #(#layout_depth_match_arms,)*
+                    _ => 0,
                 }
             }
         }
-
-
     };
 
     TokenStream::from(expanded)
