5a4ed93e15b25989429b86fa8f7ab6ede7c98c90
Sabin Regmi <get2sabin@gmail.com>
2026-04-06 14:41:09 -0400

    omx(team): auto-checkpoint worker-2 [unknown]

--- src/lib.rs
+++ src/lib.rs
@@ -105,7 +105,8 @@ pub mod animations;
 pub mod keyframes;
 pub mod manager;
 pub mod motion;
-pub mod pool;
+#[allow(dead_code)]
+pub(crate) mod pool;
 pub mod sequence;
 #[cfg(feature = "transitions")]
 pub mod transitions;
